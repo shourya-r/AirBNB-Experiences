f7d8df99f7d4d06e09a62b70c79a8f92e445930c
shourya-r <rajputshourya7@gmail.com>
2024-07-24 19:34:16 +0530

    Added and styled the navbar

---
+++ src/components/Navbar.jsx
@@ -0,0 +1,10 @@
+import airBNBLogo from "../assets/airbnb-logo.png";
+function Navbar() {
+  return (
+    <nav className="navbar">
+      <img src={airBNBLogo} className="airbnb-logo" />
+    </nav>
+  );
+}
+
+export { Navbar };
